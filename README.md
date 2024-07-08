ea17e78b055bebca744601a8fdd19ae9ba8a3827
Ben De Jonge <ben.de.jonge1996@gmail.com>
2024-07-08 11:40:04 +0200

    feat: add problem 0011

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Index | Name | Difficulty | Tags
 0001 | [Two sum](https://leetcode.com/problems/two-sum/) | Easy | array, hash table
 0003 | [Longest substring without repeating characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/) | Medium | hash table, string, sliding window
 0009 | [Palindrome numbers](https://leetcode.com/problems/palindrome-number/) | Easy | math
+0011 | [Container with the most water](https://leetcode.com/problems/container-with-most-water/description/) | Medium | array, two pointers, greedy
 0013 | [Roman to integers](https://leetcode.com/problems/roman-to-integer/) | Easy | hash table, math, string
 0014 | [Longest common prefix](https://leetcode.com/problems/longest-common-prefix/description/) | Easy | string, trie
 0020 | [Valid parentheses](https://leetcode.com/problems/valid-parentheses/description/) | Easy | string, stack
---
+++ src/bin/0011-container_with_most_water.rs
@@ -0,0 +1,60 @@
+/// https://leetcode.com/problems/container-with-most-water/description/
+/// Medium - [array, two pointer, greedy]
+/// You are given an integer array height of length n.
+/// There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+/// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+/// Return the maximum amount of water a container can store.
+/// Notice that you may not slant the container.
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn max_area_naive(height: Vec<i32>) -> i32 {
+        let mut best_area = 0;
+        for left in 0..height.len() - 1 {
+            for right in left..height.len() {
+                // The left side is so low that no matter the horizontal length, we will never beat the best area.
+                if height[left] * ((height.len() - left) as i32) < best_area {
+                    break;
+                }
+                // Compute and compare the new area.
+                let horizontal = (right - left) as i32;
+                let vertical = height[left].min(height[right]);
+                best_area = best_area.max(horizontal * vertical)
+            }
+        }
+        best_area
+    }
+
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        // Start the boundaries as the widest container.
+        let mut left = 0;
+        let mut right = height.len() - 1;
+        let mut best_area = 0;
+        while left < right {
+            // Compute the current area and overwrite the max if it is bigger.
+            best_area = best_area.max(((right - left) as i32) * height[left].min(height[right]));
+            // Greedy approach: try to improve the worst height.
+            if height[left] < height[right] {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        best_area
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_0011() {
+        assert_eq!(Solution::max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
+        assert_eq!(Solution::max_area(vec![1, 1]), 1)
+    }
+}
