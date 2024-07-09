b15b0a150d583065634157a3fc8803bc18d7b2d5
Ben De Jonge <ben.de.jonge1996@gmail.com>
2024-07-09 12:47:10 +0200

    feat: add problem 0005

--- README.md
+++ README.md
@@ -6,16 +6,28 @@ Just solvin' some LeetCode problems on the side in the best language in the worl
 
 # Currently solved
 
-Index | Name | Difficulty | Tags
------|------|------------|-----
-0001 | [Two sum](https://leetcode.com/problems/two-sum/) | Easy | array, hash table
-0003 | [Longest substring without repeating characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/) | Medium | hash table, string, sliding window
-0009 | [Palindrome numbers](https://leetcode.com/problems/palindrome-number/) | Easy | math
-0011 | [Container with the most water](https://leetcode.com/problems/container-with-most-water/description/) | Medium | array, two pointers, greedy
-0013 | [Roman to integers](https://leetcode.com/problems/roman-to-integer/) | Easy | hash table, math, string
-0014 | [Longest common prefix](https://leetcode.com/problems/longest-common-prefix/description/) | Easy | string, trie
-0020 | [Valid parentheses](https://leetcode.com/problems/valid-parentheses/description/) | Easy | string, stack
-0021 | [Merge two sorted lists]() | Easy | linked list, recursion
-0026 | [Remove duplicates from sorted array](https://leetcode.com/problems/merge-two-sorted-lists/description/) | Easy | array, two pointers
-0027 | [Remove elements](https://leetcode.com/problems/remove-element/description/) | Easy | array, two pointers
-1518 | [Water bottles](https://leetcode.com/problems/water-bottles/description/) | Easy | math simulation
+## Overview
+
+|  | Difficulty | Solved |
+|--|------------|--------|
+|🟢| Easy | 9 |
+|🟡| Medium | 3 |
+|🔴| Hard | 0 |
+|  | **Total** | **12** |
+
+## Problem list
+
+|  | Index | Name | Difficulty | Tags |
+|--|-------|------|------------|------|
+|🟢| 0001 | [Two sum](https://leetcode.com/problems/two-sum/) | Easy | array, hash table |
+|🟡| 0003 | [Longest substring without repeating characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/) | Medium | hash table, string, sliding window |
+|🟡| 0005 | [Longest palindromic substring](https://leetcode.com/problems/longest-palindromic-substring/) | Medium | two pointers, string, dynamic programming |
+|🟢| 0009 | [Palindrome numbers](https://leetcode.com/problems/palindrome-number/) | Easy | math |
+|🟡| 0011 | [Container with the most water](https://leetcode.com/problems/container-with-most-water/description/) | Medium | array, two pointers, greedy |
+|🟢| 0013 | [Roman to integers](https://leetcode.com/problems/roman-to-integer/) | Easy | hash table, math, string |
+|🟢| 0014 | [Longest common prefix](https://leetcode.com/problems/longest-common-prefix/description/) | Easy | string, trie |
+|🟢| 0020 | [Valid parentheses](https://leetcode.com/problems/valid-parentheses/description/) | Easy | string, stack |
+|🟢| 0021 | [Merge two sorted lists]() | Easy | linked list, recursion |
+|🟢| 0026 | [Remove duplicates from sorted array](https://leetcode.com/problems/merge-two-sorted-lists/description/) | Easy | array, two pointers |
+|🟢| 0027 | [Remove elements](https://leetcode.com/problems/remove-element/description/) | Easy | array, two pointers |
+|🟢| 1518 | [Water bottles](https://leetcode.com/problems/water-bottles/description/) | Easy | math simulation |
---
+++ src/bin/0005-longest_palindromic_substring.rs
@@ -0,0 +1,122 @@
+/// https://leetcode.com/problems/longest-palindromic-substring/
+/// Medium - [two pointers, string, dynamic programming]
+/// Given a string s, return the longest palindromic substring in s.
+
+pub struct Palindrome {
+    i_start: usize,
+    length: usize,
+}
+
+impl Palindrome {
+    pub fn new(length: usize, i_start: usize) -> Self {
+        Self { length, i_start }
+    }
+
+    pub fn update(&mut self, i_start: usize, length: usize) {
+        if self.length < length {
+            self.i_start = i_start;
+            self.length = length;
+        }
+    }
+
+    pub fn to_string(&self, chars: Vec<char>) -> String {
+        chars[self.i_start..self.i_start + self.length]
+            .iter()
+            .collect()
+    }
+}
+
+impl Default for Palindrome {
+    fn default() -> Self {
+        Self::new(0, 0)
+    }
+}
+
+pub struct Solution {}
+
+impl Solution {
+    fn longest_palindrome(s: String) -> String {
+        // Edge cases on length.
+        if s.is_empty() {
+            return "".to_string();
+        } else if s.len() == 1 {
+            return s.chars().next().unwrap().to_string();
+        }
+        // Constructing a vec of chars for easier iteration.
+        let chars = s.chars().collect::<Vec<char>>();
+        // We can safely set length to 1 as we know the string is not empty.
+        let mut best: Palindrome = Palindrome::new(1, 0);
+        // Construct a table representing if the string slice between bounds [left, right[ is palindromic.
+        // This requires len x len entries. This is similar to a covariance matrix!
+        let mut cache = vec![vec![false; s.len()]; s.len()];
+        // All substrings of length 1 are palindromic.
+        //          i
+        //    1  0  .  0  0
+        //    0  1  .  0  0
+        // j  .  .  .  .  .
+        //    0  0  .  1  0
+        //    0  0  .  0  1
+        for i in 0..s.len() {
+            cache[i][i] = true;
+        }
+        // Special case of length 2 substrings. Below, we will check if adding two outer characters keeps the slice a
+        // palindrome. However, for even lengthts, we then first need to know which pairs of two are palindromic.
+        for i in 0..(s.len() - 1) {
+            if chars[i] == chars[i + 1] {
+                cache[i][i + 1] = true;
+                best.update(i, 2);
+            }
+        }
+        // Checking all other lengths, starting at 3. Here, we expand both indices outwards.
+        // Make length inclusive, as we also need to test for the whole string being palindromic.
+        for length in 3..=s.len() {
+            // Looping over starting positions.
+            for i in 0..s.len() - length + 1 {
+                // The ending index, so we need minus 1 because we are using substring length.
+                let j = i + length - 1;
+                // It was a palindrome before we added the two outer characters.
+                if cache[i + 1][j - 1]
+				// The two outer characters are the same, so it is still a palindrome.
+				&& chars[i] == chars[j]
+                {
+                    cache[i][j] = true;
+                    best.update(i, length);
+                }
+            }
+        }
+        best.to_string(chars)
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_0005() {
+        assert_eq!(
+            Solution::longest_palindrome("babad".to_string()),
+            "bab".to_string()
+        );
+        assert_eq!(
+            Solution::longest_palindrome("cbbd".to_string()),
+            "bb".to_string()
+        );
+        assert_eq!(
+            Solution::longest_palindrome("ccc".to_string()),
+            "ccc".to_string()
+        );
+        assert_eq!(
+            Solution::longest_palindrome("ac".to_string()),
+            "a".to_string()
+        );
+        assert_eq!(
+            Solution::longest_palindrome("a".to_string()),
+            "a".to_string()
+        );
+        assert_eq!(Solution::longest_palindrome("".to_string()), "".to_string());
+    }
+}
