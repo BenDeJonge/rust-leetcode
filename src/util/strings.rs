84b1be7081888348d5fb68a50234e55e912ac733
Ben De Jonge <ben.de.jonge1996@gmail.com>
2024-07-16 13:15:35 +0200

    feat: add string methods

---
+++ src/util/strings.rs
@@ -0,0 +1,8 @@
+pub fn char_windows(src: &str, win_size: usize) -> impl Iterator<Item = &str> {
+    src.char_indices().flat_map(move |(from, _)| {
+        src[from..]
+            .char_indices()
+            .nth(win_size - 1)
+            .map(|(to, c)| &src[from..from + to + c.len_utf8()])
+    })
+}
