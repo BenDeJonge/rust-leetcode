4cf54cc27c9b7100f767847b27eef1daa619a3fa
Ben De Jonge <ben.de.jonge1996@gmail.com>
2024-07-24 12:51:25 +0200

    docs: explanatory comments for TreeNode::from_vec

--- src/util/binary_tree.rs
+++ src/util/binary_tree.rs
@@ -9,6 +9,7 @@
 /// convenient to use a RefCell<T>, which allows interior mutability i.e., mutating the contents through a immutable
 /// reference &T instead of an exclusive reference &mut T. Borrows are still checked at runtime to ensure memory safety.
 use std::cell::RefCell;
+use std::fmt::Debug;
 use std::rc::Rc;
 pub type TreeNodeRef<T> = Rc<RefCell<TreeNode<T>>>;
 pub type OptNode<T> = Option<TreeNodeRef<T>>;
@@ -37,25 +38,33 @@ impl<T> TreeNode<T> {
     // https://github.com/pymongo/leetcode-rust/blob/master/src/binary_tree/serde_binary_tree_to_leetcode_vec.rs#L42
     pub fn from_vec(data: &[Option<T>]) -> OptNode<T>
     where
-        T: Copy,
+        T: Copy + Debug,
     {
         if data.is_empty() {
             return None;
         }
-
+        // Grabbing all the nodes.
         let nodes = data
             .iter()
             .map(|&val| val.and_then(|v| TreeNode::new_optnode(v)))
             .collect::<Vec<OptNode<T>>>();
         let root = nodes.first()?.clone();
+        // We have already found a place for the root, so start at 1 instead of 0.
         let mut child_node_ptr = 1;
+        // Flatten the iterator to only deal with Some values i.e., only associated child nodes to nodes with children.
         for node in nodes.iter().flatten() {
+            // There are still child nodes remaining, but these can be None as well.
             if let Some(child_node) = nodes.get(child_node_ptr) {
+                // Clone the child node while reusing the resources from the source to avoid unneeded reallocations.
                 node.borrow_mut().left.clone_from(child_node);
+                // This node has received a parent, so let's not look at it again.
                 child_node_ptr += 1;
-            } else {
+            }
+            // We have exceeded the list of the child nodes and any remaining nodes do not have children.
+            else {
                 break;
             }
+            // Same logic for the right nodes.
             if let Some(child_node) = nodes.get(child_node_ptr) {
                 node.borrow_mut().right.clone_from(child_node);
                 child_node_ptr += 1;
