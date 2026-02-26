c5ef4c31a7d1b4aaa552096cab4c723b403f7252
Ben De Jonge <ben.de.jonge1996@gmail.com>
2026-02-26 23:16:18 +0100

    feat: cleanup s0034

--- src/rust_leetcode/medium/s0034_find_first_and_last_position_of_element_in_sorted_array.rs
+++ src/rust_leetcode/medium/s0034_find_first_and_last_position_of_element_in_sorted_array.rs
@@ -32,29 +32,39 @@ enum Bound {
 pub struct Solution {}
 
 impl Solution {
+    /// An ideomatic solution using standard functions.
+    pub fn search_range_ideomatic<T: Ord>(nums: &[T], target: T) -> Option<[usize; 2]> {
+        nums.binary_search(&target)
+            .map(|i| {
+                [
+                    nums[..i].partition_point(|val| val < &target),
+                    i + nums[i..].partition_point(|val| val <= &target) - 1,
+                ]
+            })
+            .ok()
+    }
+
     /// Use a manual binary search to identify the ranges within which
     /// to search for the upper and lower bounds.
     /// - Time complexity: `O(log(n)) + 2 * O(log(n / 2)) = O(log(n))`
-    /// - Space complexity: `O(1)`
+    /// - Space complexity: `O(1)`  
     pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        Solution::search_range_ref(&nums, target).to_vec()
+        Solution::search_range_manual(&nums, target)
+            .map(|arr| vec![arr[0] as i32, arr[1] as i32])
+            .unwrap_or(vec![-1, -1])
     }
 
-    // Leetcode enforces poor Rust typing.
-    fn search_range_ref(nums: &[i32], target: i32) -> [i32; 2] {
-        if nums.is_empty() {
-            return [-1, -1];
-        }
-
-        let mut left = 0usize;
-        let mut right = nums.len() - 1;
+    // An manual version that implements binary search and partition point.
+    fn search_range_manual<T: Ord>(nums: &[T], target: T) -> Option<[usize; 2]> {
+        let mut left = 0i32;
+        let mut right = nums.len() as i32 - 1;
         let mut i = None;
         let mut i_min = left;
         let mut i_max = right;
 
         while left <= right {
-            let middle = left + (right - left) / 2;
-            match nums[middle].cmp(&target) {
+            let middle = (left + right) / 2;
+            match nums[middle as usize].cmp(&target) {
                 Ordering::Less => {
                     left = middle + 1;
                     i_min = i_min.max(middle);
@@ -71,55 +81,21 @@ impl Solution {
         }
 
         if let Some(i) = i {
-            [
-                (i_min + nums[i_min..i].partition_point(|&num| num < target)) as i32,
-                (i + nums[i..i_max].partition_point(|&num| num <= target) - 1) as i32,
-            ]
+            let i_usize = i as usize;
+            let i_min_usize = i_min as usize;
+            let i_max_usize = i_max as usize;
+            Some([
+                i_min_usize
+                    + (Solution::get_bound(&nums[i_min_usize..=i_usize], &target, Bound::Left)),
+                i_usize
+                    + (Solution::get_bound(&nums[i_usize..=i_max_usize], &target, Bound::Right)),
+            ])
         } else {
-            [-1, -1]
+            None
         }
     }
 
-    fn search_range_manual(nums: &[i32], target: i32) -> Vec<i32> {
-        if nums.is_empty() {
-            return vec![-1, -1];
-        }
-
-        let mut left = 0usize;
-        let mut right = nums.len() - 1;
-        let mut i = None;
-        let mut i_min = left;
-        let mut i_max = right;
-
-        while left <= right {
-            let middle = left + (right - left) / 2;
-            match nums[middle].cmp(&target) {
-                Ordering::Less => {
-                    left = middle + 1;
-                    i_min = i_min.max(middle);
-                }
-                Ordering::Equal => {
-                    i = Some(middle);
-                    break;
-                }
-                Ordering::Greater => {
-                    right = middle - 1;
-                    i_max = i_max.min(middle);
-                }
-            }
-        }
-
-        if let Some(i) = i {
-            vec![
-                (i_min + Solution::get_bound(&nums[i_min..i], target, Bound::Left)) as i32,
-                (i + Solution::get_bound(&nums[i..i_max], target, Bound::Right)) as i32,
-            ]
-        } else {
-            vec![-1, -1]
-        }
-    }
-
-    fn get_bound(nums: &[i32], target: i32, bound: Bound) -> usize {
+    fn get_bound<T: Ord>(nums: &[T], target: &T, bound: Bound) -> usize {
         if nums.is_empty() {
             return 0;
         }
@@ -127,16 +103,19 @@ impl Solution {
         let mut right = nums.len() - 1;
 
         while left < right {
-            let middle = left + (right - left) / 2;
+            let mut middle = (left + right) / 2;
             match bound {
-                Bound::Left => match nums[middle].cmp(&target) {
+                Bound::Left => match nums[middle].cmp(target) {
                     Ordering::Less => left = middle + 1,
-                    Ordering::Equal | Ordering::Greater => right = middle - 1,
+                    Ordering::Equal | Ordering::Greater => right = middle,
                 },
-                Bound::Right => match nums[middle].cmp(&target) {
-                    Ordering::Less | Ordering::Equal => left = middle + 1,
-                    Ordering::Greater => right = middle - 1,
-                },
+                Bound::Right => {
+                    middle += 1;
+                    match nums[middle].cmp(target) {
+                        Ordering::Less | Ordering::Equal => left = middle,
+                        Ordering::Greater => right = middle - 1,
+                    }
+                }
             }
         }
         match bound {
@@ -153,18 +132,19 @@ mod tests {
     #[test]
     fn test_0034() {
         for (nums, target, expected) in [
-            // (vec![5, 7, 7, 8, 8, 10], 8, [3, 4]),
-            // (vec![7, 7, 8, 8, 9, 10], 7, [0, 1]),
-            // (vec![5, 7, 7, 8, 8, 10], 6, [-1, -1]),
-            // (vec![], 0, [-1, -1]),
-            (vec![1], 0, [-1, -1]),
+            (vec![5, 7, 7, 8, 8, 10], 8, Some([3, 4])),
+            (vec![7, 7, 8, 8, 9, 10], 7, Some([0, 1])),
+            (vec![5, 7, 7, 8, 8, 10], 6, None),
+            (vec![2, 2], 2, Some([0, 1])),
+            (vec![], 0, None),
+            (vec![1], 0, None),
         ] {
             helper(&nums, target, expected);
         }
     }
 
-    fn helper(nums: &[i32], target: i32, expected: [i32; 2]) {
-        assert_eq!(Solution::search_range_ref(nums, target), expected);
+    fn helper(nums: &[i32], target: i32, expected: Option<[usize; 2]>) {
         assert_eq!(Solution::search_range_manual(nums, target), expected);
+        assert_eq!(Solution::search_range_ideomatic(nums, target), expected);
     }
 }
